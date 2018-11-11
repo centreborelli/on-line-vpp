02c165d7158e3b2500c1e29e3a51cd13531d923d
Anger Jérémy <angerj.dev@gmail.com>
2018-11-11 21:14:27 +0000

    kalman: read sigmas from a vpp stream

--- src/4_denoising/kalman/main.c
+++ src/4_denoising/kalman/main.c
@@ -28,6 +28,7 @@ int main(int argc, const char *argv[])
 	const char *occl_path = NULL;
 	const char *deno_path = NULL; // output stream
 	int fframe = 0, lframe = -1;
+	const char* sigma_path = NULL;
 	float sigma = 0.f;
 	bool verbose = false;
 	struct nlkalman_params prms;
@@ -49,7 +50,7 @@ int main(int argc, const char *argv[])
 		OPT_STRING ('d', "deno"  , &deno_path, "denoised output path (printf format)"),
 		OPT_INTEGER('f', "first" , &fframe, "first frame"),
 		OPT_INTEGER('l', "last"  , &lframe , "last frame"),
-		OPT_FLOAT  ('s', "sigma" , &sigma, "noise standard dev"),
+		OPT_STRING ('s', "sigma" , &sigma_path, "noise standard dev"),
 		OPT_INTEGER('p', "patch" , &prms.patch_sz, "patch size"),
 		OPT_INTEGER('w', "search", &prms.search_sz, "search region radius"),
 		OPT_FLOAT  ( 0 , "dth"   , &prms.dista_th, "patch distance threshold"),
@@ -68,33 +69,28 @@ int main(int argc, const char *argv[])
 	argparse_describe(&argparse, "\nA video denoiser based on non-local means.", "");
 	argc = argparse_parse(&argparse, argc, argv);
 
-	// default value for noise-dependent params
-	nlkalman_default_params(&prms, sigma);
-
-	// print parameters
-	if (verbose)
-	{
-		printf("parameters:\n");
-		printf("\tnoise  %f\n", sigma);
-		printf("\t%s-wise mode\n", prms.pixelwise ? "pixel" : "patch");
-		printf("\tpatch     %d\n", prms.patch_sz);
-		printf("\tsearch    %d\n", prms.search_sz);
-		printf("\tdth       %g\n", prms.dista_th);
-		printf("\tlambda    %g\n", prms.dista_lambda);
-		printf("\tbeta_x    %g\n", prms.beta_x);
-		printf("\tbeta_t    %g\n", prms.beta_t);
-		printf("\n");
-#ifdef WEIGHTED_AGGREGATION
-		printf("\tWEIGHTED_AGGREGATION ON\n");
-#endif
-	}
-
 	// process first frame [[[2
 	int w,h,c;
 	FILE* nisy_in = vpp_init_input(nisy_path, &w, &h, &c);
 	if (!nisy_in)
 	{
 		fprintf(stderr, "kalman: cannot initialize input '%s'\n", nisy_path);
+		return EXIT_FAILURE;
+	}
+	int nbbinssigma;
+	int ncsigma;
+	int unused;
+	FILE* sigma_in = vpp_init_input(sigma_path, &ncsigma, &nbbinssigma, &unused);
+	float sigmadata[2]; // stores mean and stdev
+	if (!sigma_in)
+	{
+		fprintf(stderr, "kalman: cannot initialize sigma '%s'\n", sigma_path);
+		return EXIT_FAILURE;
+	}
+	if (ncsigma != 2 || nbbinssigma != 1 || unused != 1)
+	{
+		fprintf(stderr, "kalman: invalid sigma stream dimensions: %dx%dx%dx\n",
+				ncsigma, nbbinssigma, unused);
 		return EXIT_FAILURE;
 	}
 
@@ -110,8 +106,30 @@ int main(int argc, const char *argv[])
 	float* nisy1 = malloc(whc*sizeof(float));
 	float* deno1 = malloc(whc*sizeof(float));
 
+	// default value for noise-dependent params
+	vpp_read_frame(sigma_in, sigmadata, ncsigma, nbbinssigma, 1);
+	sigma = sigmadata[1];
+	nlkalman_default_params(&prms, sigma);
+
+	// print parameters
+	if (verbose)
+	{
+		fprintf(stderr, "parameters:\n");
+		fprintf(stderr, "\tnoise  %f\n", sigma);
+		fprintf(stderr, "\t%s-wise mode\n", prms.pixelwise ? "pixel" : "patch");
+		fprintf(stderr, "\tpatch     %d\n", prms.patch_sz);
+		fprintf(stderr, "\tsearch    %d\n", prms.search_sz);
+		fprintf(stderr, "\tdth       %g\n", prms.dista_th);
+		fprintf(stderr, "\tlambda    %g\n", prms.dista_lambda);
+		fprintf(stderr, "\tbeta_x    %g\n", prms.beta_x);
+		fprintf(stderr, "\tbeta_t    %g\n", prms.beta_t);
+		fprintf(stderr, "\n");
+#ifdef WEIGHTED_AGGREGATION
+		fprintf(stderr, "\tWEIGHTED_AGGREGATION ON\n");
+#endif
+	}
 	// run denoising
-	if (verbose) printf("processing frame %d\n", 0);
+	if (verbose) fprintf(stderr, "processing frame %d\n", 0);
 	vpp_read_frame(nisy_in, nisy1, w, h, c);
 	nlkalman_frame(deno1, nisy1, NULL, w, h, c, sigma, prms, 0);
 	vpp_write_frame(deno_out, deno1, w, h, c);
@@ -122,7 +140,7 @@ int main(int argc, const char *argv[])
 	FILE* flow_in = NULL;
 	if (flow_path)
 	{
-		if (verbose) printf("loading flow %s\n", flow_path);
+		if (verbose) fprintf(stderr, "loading flow %s\n", flow_path);
 		int w1, h1, c1;
 		flow_in = vpp_init_input(flow_path, &w1, &h1, &c1);
 
@@ -143,7 +161,7 @@ int main(int argc, const char *argv[])
 	FILE* occl_in = NULL;
 	if (flow_path && occl_path)
 	{
-		if (verbose) printf("loading occl. mask %s\n", occl_path);
+		if (verbose) fprintf(stderr, "loading occl. mask %s\n", occl_path);
 		int w1, h1, c1;
 		occl_in = vpp_init_input(occl_path, &w1, &h1, &c1);
 
@@ -168,7 +186,7 @@ int main(int argc, const char *argv[])
 	int f = 1;
 	while (vpp_read_frame(nisy_in, nisy1, w, h, c))
 	{
-		if (verbose) printf("processing frame %d\n", f);
+		if (verbose) fprintf(stderr, "processing frame %d\n", f);
 
 		// warp previous denoised frame
 		if (flow10)
@@ -180,6 +198,11 @@ int main(int argc, const char *argv[])
 		else
 			// copy without warping
 			memcpy(warp0, deno1, whc*sizeof(float));
+
+		// read sigma and change parameters
+		vpp_read_frame(sigma_in, sigmadata, ncsigma, nbbinssigma, 1);
+		sigma = sigmadata[1];
+		nlkalman_default_params(&prms, sigma);
 
 		// run denoising
 		nlkalman_frame(deno1, nisy1, warp0, w, h, c, sigma, prms, f++);

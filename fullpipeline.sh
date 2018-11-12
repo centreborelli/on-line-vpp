5aa75eb51937d64647e16b279be161fe0f9f9dd8
Anger Jérémy <angerj.dev@gmail.com>
2018-11-12 09:09:56 +0000

    pipeline: stabilization before denoising

--- fullpipeline.sh
+++ fullpipeline.sh
@@ -16,14 +16,14 @@ bin/readvid "$1" - \
 	| ./src/1_preprocessing/remove_fpn.m - - \
 	| ./src/1_preprocessing/unband/unband.m - - \
 	| ./bin/vp dup - - $pono \
+	`# stabilization` \
+	| bin/estadeo - -o - \
 	`# nlkalman (with opticalflow and occlusion detection)` \
 	| bin/vp dup - $noisy - \
 	| bin/tvl1flow - - \
 	| bin/vp dup - $oflow - \
 	| bin/vlambda - "x(0,0)[0] x(-1,0)[0] - x(0,0)[1] x(0,-1)[1] - + 0.5 > 255 *" -o - \
 	| bin/kalman -i $noisy -o $oflow -k - -s $sigmas -d - -v \
-	`# stabilization` \
-	| bin/estadeo - -o - \
 	`# tonemapping` \
 	| ./src/6_tonemapping/tonemapping.m - - \
 	`# save` \

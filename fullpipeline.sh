9991af32f3afc219655a770e01c6714866d9752e
parias <pariasm@gmail.com>
2018-11-12 11:06:13 +0100

    fullpipeline.sh: add tvl1 parameters

--- fullpipeline.sh
+++ fullpipeline.sh
@@ -11,6 +11,11 @@ mkfifo $oflow
 mkfifo $pono
 mkfifo $sigmas
 
+# optical flow parameters
+DW=0.1 # weight of data attachment term 0.1 ~ very smooth 0.2 ~ noisy
+FS=1   # finest scale (0 image scale, 1 one coarser level, 2 more coarse, etc...
+oflow_params="0 0.25 $DW 0.3 100 $FS 0.5 5 0.01 0" 
+
 bin/readvid "$1" - \
 	`# preprocessing` \
 	| ./src/1_preprocessing/remove_fpn.m - - \
@@ -20,7 +25,7 @@ bin/readvid "$1" - \
 	| bin/estadeo - -o - \
 	`# nlkalman (with opticalflow and occlusion detection)` \
 	| bin/vp dup - $noisy - \
-	| bin/tvl1flow - - \
+	| bin/tvl1flow - - $oflow_params \
 	| bin/vp dup - $oflow - \
 	| bin/vlambda - "x(0,0)[0] x(-1,0)[0] - x(0,0)[1] x(0,-1)[1] - + 0.5 > 255 *" -o - \
 	| bin/kalman -i $noisy -o $oflow -k - -s $sigmas -d - -v \

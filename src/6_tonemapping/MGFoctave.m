a67384fc5a7936491e020d8db1169c40abe2ea68
Charles Hessel <hesselcharles@gmail.com>
2018-12-13 21:06:12 +0100

    tone-mapping: cleanup

--- src/6_tonemapping/MGFoctave.m
+++ src/6_tonemapping/MGFoctave.m
@@ -1,33 +1,16 @@
 function R = MGFoctave( I, G, epsilon, radius, nIt, nScales, gamma )
 % MGFoctave apply the multi-scale guided filter.
+% R = MGFoctave( I, G, epsilon, radius, nIt, nScales, gamma )
 %
-% a_ms_igfcV2 is the multiscale (ms) iterated guided filter V2 with a color
-% guide (igfcV2).
-% This filter is described in the thesis, chapter "iterated guided filter".
-% The filter also works with gray guide images. 
-%
-% This filter creates an artifact know under the name of dark halo
-% artifact. This is studied in the last chapter of the thesis.
-%
-% NOTES:
-% >> must be in the Matlab PATH:
-%    1) ~/thesis/Work_In_Progress/myfunc_guided_IteratedGuidedFilter_Supp/
-%       For igfV2 and igfcV2.
-%    2) ~/thesis/Work_In_Progress/171002_fastresetgf/ For the (fast)
-%       functions used for the Gaussian and Laplacian pyramids.
-%
-% Sunday, December 31, 2017.
-% La Permanence, 4 rue du Fer \'{a} Moulin, 75005 Paris
+% Charles Hessel -- CMLA, ENS-Paris-Saclay, 2018
 
 if nScales==0
-    % nScales = max(1, floor(log2(min(size(I,1),size(I,2)))) -2);
-    %%% version below is easier to justify
     nScales = max(1, floor(log2(min(size(I,1),size(I,2)))) );
 elseif nScales < 0
     nScales = max(1, floor(log2(min(size(I,1),size(I,2)))) -nScales);
 end
 if nargin == 6
-    gamma = 1/3;
+    gamma = 1;
 end
 
 if ( size(I,3) == 3 )
@@ -42,8 +25,7 @@ filter = pyramid_filter;
 
 % Compute gaussian and laplacian pyramids of the input and guide images.
 [~,I_lpyr] = glpyr(I,nScales);
-[G_gpyr, G_lpyr] = glpyr(G,nScales);
-% G_gpyr     = gpyr(G,nScales);
+G_gpyr = gpyr(G,nScales);
 
 % Initialise the output R with the residual of the laplacian pyramid
 R = I_lpyr{nScales};
@@ -51,9 +33,9 @@ R = I_lpyr{nScales};
 % Filter the input image scale by scale starting from the coarsest one
 % fprintf('Scale %02d: ',nScales);
 if color
-    [R, A] = a_igfcV2( R, G_gpyr{nScales}, epsilon/nScales^gamma, radius, nIt );
+    R = igfcV2( R, G_gpyr{nScales}, epsilon/nScales^gamma, radius, nIt );
 else
-    [R, A] = a_igfV2(  R, G_gpyr{nScales}, epsilon/nScales^gamma, radius, nIt );
+    R = igfV2(  R, G_gpyr{nScales}, epsilon/nScales^gamma, radius, nIt );
 end
 
 % Continue to recursively upsample, add coefficients and filter the image.
@@ -63,20 +45,14 @@ for scale = nScales-1:-1:1
     odd = 2*size(R) - size(I_lpyr{scale});
     
     % Upsample previously filtered image, add Laplacian coefficients
-    % A = max(0, fastupsample(A,odd,filter) );
-    % A = ones(size(G_lpyr{scale}));
-    % L  = G_lpyr{scale}; % Why using the Laplacian coeffs of the guide?
-    L  = I_lpyr{scale}; % Laplacian coefficients of the input image
-    % LC = zeros(size(L(:,:,1)));
-    % for c = 1:size(G,3), LC = LC + A(:,:,c).*L(:,:,c); end
-    R = fastupsample(R,odd,filter) + L; %LC; % instead of I_lpyr
+    R = fastupsample(R,odd,filter) + I_lpyr{scale};
     
     % Filter
-%     fprintf('Scale %02d: ',scale);
+    % fprintf('Scale %02d: ',scale);
     if color
-        [R, A] = a_igfcV2( R, G_gpyr{scale}, epsilon/scale^gamma, radius, nIt );
+        R = igfcV2( R, G_gpyr{scale}, epsilon/scale^gamma, radius, nIt );
     else
-        [R, A] = a_igfV2(  R, G_gpyr{scale}, epsilon/scale^gamma, radius, nIt );
+        R = igfV2(  R, G_gpyr{scale}, epsilon/scale^gamma, radius, nIt );
     end
 end
 
@@ -84,19 +60,19 @@ end
 % pass filter for constructing Gaussian and Laplacian pyramids.
 %
 % tom.mertens@gmail.com, August 2007
-%
 
-function f = pyramid_filter;
+function f = pyramid_filter
 f = [.0625, .25, .375, .25, .0625];
 
+% Simultaneous construction of Gaussian and Laplacian pyramid.
+% Save computations when both are needed.
+% Use gpyr to compute the gaussian pyramid only.
+%
+% This is base on a function by Tom Mertens.
+%
+% Charles Hessel, 2018
+
 function [gpyr,lpyr] = glpyr(I,nlev)
-
-r = size(I,1);
-c = size(I,2);
-
-if ~exist('nlev','var') % compute the highest possible pyramid    
-    nlev = floor( log(min(r,c)) / log(2) );
-end
 
 % recursively build pyramid
 gpyr = cell(nlev,1);
@@ -106,16 +82,29 @@ J = I;
 for l = 1:nlev - 1
     % apply low pass filter, and downsample
     I = fastdownsample(J,filter);
-    % CH: 5th Jan 2018: I modified the function because there was an error
-    % CH: line below: it was gpyr{l} = I;
     gpyr{l} = J;
-    odd = 2*size(I) - size(J);  % for each dimension, check if the upsampled version has to be odd
-    % in each level, store difference between image and upsampled low pass version
+    % for each dimension, check if the upsampled version has to be odd
+    odd = 2*size(I) - size(J);
+    % in each level, store diff between image and upsampled low pass version
     lpyr{l} = J - fastupsample(I,odd,filter);
     J = I; % continue with low pass image
 end
 gpyr{nlev} = J; % the coarest level contains the residual low pass image
 lpyr{nlev} = J;
+
+function gpyr = gpyr(I,nlev)
+
+% recursively build pyramid
+gpyr = cell(nlev,1);
+filter = pyramid_filter;
+J = I;
+for l = 1:nlev - 1
+    % apply low pass filter, and downsample
+    I = fastdownsample(J,filter);
+    gpyr{l} = J;
+    J = I; % continue with low pass image
+end
+gpyr{nlev} = J; % the coarest level contains the residual low pass image
 
 % Upsampling procedure.
 %
@@ -129,7 +118,7 @@ lpyr{nlev} = J;
 % Same for height.
 %
 % tom.mertens@gmail.com, August 2007
-%
+% Charles Hessel, 2018
 
 function R = fastupsample(I,odd,filter)
 
@@ -139,20 +128,9 @@ r = 2*size(I,1);
 c = 2*size(I,2);
 k = size(I,3);
 
-% Commented for replacement by (below)
-% R = zeros(r,c,k);
-% R(1:2:r, 1:2:c, :) = 4*I; % increase size 2 times; the padding is now 2 pixels wide
-% 
-% % interpolate, convolve with separable filter
-% R = imfilter(R,filter);     %horizontal
-% R = imfilter(R,filter');    %vertical
-
-Rt = zeros(r/2,c,k);
-Rt(:, 1:2:c, :) = 4*I;
-Rt = imfilter(Rt,filter); % horizontal
-R = zeros(r,c,k);
-R(1:2:r, :, :) = Rt;
-R = imfilter(R,filter'); % vertical
+% horizontal, then vertical
+Rt = zeros(r/2,c,k); Rt(:, 1:2:c, :) = 4*I; Rt = imfilter(Rt,filter);
+R  = zeros(r  ,c,k);  R(1:2:r, :, :) = Rt;   R = imfilter(R,filter');
 
 % remove the border
 R = R(3:r - 2 - odd(1), 3:c - 2 - odd(2), :);
@@ -173,7 +151,7 @@ R = imfilter(R,filter',border_mode); %vertical
 % decimate
 R = R(1:2:end,:,:);
 
-function [R, A] = a_igfcV2( I, G, epsilon, radius, T )
+function R = igfcV2( I, G, epsilon, radius, T )
 % igfcV2 is the iterated guided filter V2 with color guide
 % (igfcV1 with unmodified guide).
 %
@@ -185,11 +163,11 @@ function [R, A] = a_igfcV2( I, G, epsilon, radius, T )
 R = I; % initialisation
 
 for t=1:T
-%     m = textprogressbar( t/T ,m, 50 );
-    [R, A] = a_gfc( R, epsilon, radius, G );
+    % m = textprogressbar( t/T ,m, 50 );
+    R = gfc( R, epsilon, radius, G );
 end
 
-function [R, A] = a_igfV2( I, G, epsilon, radius, T )
+function R = igfV2( I, G, epsilon, radius, T )
 
 % igfV2 is the iterated guided filter V2 (igfV1 with unmodified guide).
 % see thesis document. 
@@ -210,28 +188,16 @@ function [R, A] = a_igfV2( I, G, epsilon, radius, T )
 % \end{equation}
 % with $R_{\text{IGF/v2}}^0 = I$.
 
-% fprintf('a_igfV2: ');
+% fprintf('igfV2: ');
 % m = textprogressbar( 0 ,0, 50 );
 R = I; % initialisation
 
 for t=1:T
-%     m = textprogressbar( t/T ,m, 50 );
-    % [R, ~] = a_gf( R, epsilon, radius, G ); % <-- officiel
-    [R, ~] = a_gf( R, epsilon, radius*2^(t-1), G );
+    % m = textprogressbar( t/T ,m, 50 );
+    R = gf( R, epsilon, radius, G );
 end
 
-radius = 2; % <-- replace parameter radius by fixed value 2... or 1
-w      = 2*radius+1;
-H      = ones(w)./(w^2); 
-R_mean = imfilter(R   , H, 'symmetric');
-G_mean = imfilter(G   , H, 'symmetric');
-G_var  = imfilter(G.^2, H, 'symmetric') - G_mean.^2;
-RG_cov = imfilter(R.*G, H, 'symmetric') - R_mean .* G_mean;
-a      = RG_cov ./ ( G_var + 1e-12 ); % <-- replaced epsilon by eps
-A      = imfilter(a   , H, 'symmetric');
-
-
-function [R, a_mean] = a_gf( I, epsilon, radius, G )
+function R = gf( I, epsilon, radius, G )
 % gf is the guided filter.
 % R = gf( I, epsilon, radius );
 % OR
@@ -267,8 +233,7 @@ switch nargin
         error('Incorrect number of arguments.')
 end
 
-
-function [R, am] = a_gfc( I, epsilon, radius, G )
+function R = gfc( I, epsilon, radius, G )
 % gfc is the guided filter with a color guide.
 % R = gfc( I, epsilon, radius, G )
 
@@ -318,11 +283,11 @@ R     =   am(:,:,1).*G(:,:,1)...
         + am(:,:,3).*G(:,:,3)...
         + bm;
 
-function msg_length = textprogressbar( x ,prev_msg_length, barlength )
-
-ers         = repmat('\b',1,prev_msg_length);
-n           = round(x*barlength);
-bar         = sprintf('[%s%s]\n',repmat('#',1,n),...
-                                 repmat(' ',1,barlength-n));
-msg_length  = length(bar);
-fprintf([ers bar]);
+% function msg_length = textprogressbar( x ,prev_msg_length, barlength )
+% 
+% ers         = repmat('\b',1,prev_msg_length);
+% n           = round(x*barlength);
+% bar         = sprintf('[%s%s]\n',repmat('#',1,n),...
+%                                  repmat(' ',1,barlength-n));
+% msg_length  = length(bar);
+% fprintf([ers bar]);
